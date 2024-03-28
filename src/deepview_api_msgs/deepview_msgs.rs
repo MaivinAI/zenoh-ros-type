8518af4d9ae2a6a1745712ec511aeed25c123724
Sébastien Taylor <sebastien@au-zone.com>
2024-03-27 19:55:25 -0600

    Migrated radarcube (with name corrections) to deepview_api_msgs (also to be renamed).

--- src/deepview_api_msgs/deepview_msgs.rs
+++ src/deepview_api_msgs/deepview_msgs.rs
@@ -12,3 +12,25 @@ pub struct DeepviewDMABuf {
     pub fourcc: u32,
     pub length: u32,
 }
+
+#[derive(Serialize, Deserialize, PartialEq, Clone)]
+pub struct RadarCube {
+    pub header: std_msgs::Header,
+    pub timestamp: u64,
+    pub frame_id: u32,
+    pub layout: Vec<u8>,
+    pub shape: Vec<u16>,
+    pub scales: Vec<f32>,
+    pub cube: Vec<i16>,
+    pub is_complex: bool,
+}
+
+pub mod radar_cube_dimension {
+    pub const UNDEFINED: u8 = 0;
+    pub const RANGE: u8 = 1;
+    pub const DOPPLER: u8 = 2;
+    pub const AZIMUTH: u8 = 3;
+    pub const ELEVATION: u8 = 4;
+    pub const RXCHANNEL: u8 = 5;
+    pub const SEQUENCE: u8 = 6;
+}
